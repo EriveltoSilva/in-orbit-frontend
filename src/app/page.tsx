501f264e955b531de61ba98d6f3da68f414bd119
eriveltosilva <eriveltoclenio@gmail.com>
2024-09-16 00:47:11 -0300

    feat: goal list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ import { GoalsList } from "@/components/goals-list";
 
 export default function Home() {
   const goals = [0];
-  return <>{goals.length === 0 ? <Empty /> : <GoalsList />}</>;
+  return <>{goals.length === 0 ? <Empty /> : <GoalsList goals={[""]} />}</>;
 }
--- src/components/goals-list/index.tsx
+++ src/components/goals-list/index.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
-import { Plus } from "lucide-react";
+import { CheckCircle2, Plus } from "lucide-react";
 
 import logo from "@/assets/images/logo.svg";
 import { Divider } from "../Divider";
 import { ProgressBar } from "../ProgressBar";
 
-export const GoalsList = () => {
+export const GoalsList = ({ goals }: { goals: string[] }) => {
   return (
     <main className="h-full py-10 flex flex-col gap-8 items-center">
       <div className="w-[480px] space-y-6">
@@ -23,10 +23,13 @@ export const GoalsList = () => {
 
         <div className="space-y-3">
           <ProgressBar percentage={60} />
-          <p className="text-xs text-zinc-400">
-            Você completou <span className="text-zinc-100">8</span> de <span className="text-zinc-100">15</span> metas
-            nessa semana
-          </p>
+          <div className="flex justify-between items-center text-xs text-zinc-400">
+            <p>
+              Você completou <span className="text-zinc-100">8</span> de <span className="text-zinc-100">15</span> metas
+              nessa semana
+            </p>
+            <span>60%</span>
+          </div>
         </div>
 
         <Divider />
@@ -48,7 +51,54 @@ export const GoalsList = () => {
 
         <div className="space-y-6">
           <h2 className="text-zinc-100 font-medium text-xl">Sua semana</h2>
-          <p className="text-zinc-400 text-sm">Você ainda não completou nenhuma meta essa semana</p>
+          {goals && goals.length === 0 && (
+            <p className="text-zinc-400 text-sm">Você ainda não completou nenhuma meta essa semana</p>
+          )}
+          <article className="space-y-4">
+            <h3 className="text-zinc-50 text-base">
+              Hoje <span className="text-zinc-400 text-xs">(06 de Agosto)</span>
+            </h3>
+            <ul className="space-y-3">
+              <li className="flex items-center gap-1 text-zinc-400 text-sm">
+                <CheckCircle2 size={16} className="text-pink-400" />
+                Você completou <span className="text-zinc-100">&quot;Acordar cedo&quot;</span> às &quot;
+                <span className="text-zinc-100">10h:30</span>&quot;
+                <button className="text-zinc-500 text-xs underline self-end">Desfazer</button>
+              </li>
+              <li className="flex items-center gap-1 text-zinc-400 text-sm">
+                <CheckCircle2 size={16} className="text-pink-400" />
+                Você completou <span className="text-zinc-100">&quot;Meditar&quot;</span> às &quot;
+                <span className="text-zinc-100">10h:30</span>&quot;
+                <button className="text-zinc-500 text-xs underline self-end">Desfazer</button>
+              </li>
+              <li className="flex items-center gap-1 text-zinc-400 text-sm">
+                <CheckCircle2 size={16} className="text-pink-400" />
+                Você completou <span className="text-zinc-100">&quot;Praticar Exercícios&quot;</span> às &quot;
+                <span className="text-zinc-100">10h:30</span>&quot;
+                <button className="text-zinc-500 text-xs underline self-end">Desfazer</button>
+              </li>
+            </ul>
+          </article>
+
+          <article className="space-y-4">
+            <h3 className="text-zinc-50 text-base">
+              Ontem <span className="text-zinc-400 text-xs">(05 de Agosto)</span>
+            </h3>
+            <ul className="space-y-3">
+              <li className="flex items-center gap-1 text-zinc-400 text-sm">
+                <CheckCircle2 size={16} className="text-pink-400" />
+                Você completou <span className="text-zinc-100">&quot;Acordar cedo&quot;</span> às &quot;
+                <span className="text-zinc-100">08:17</span>&quot;
+                <button className="text-zinc-500 text-xs underline self-end">Desfazer</button>
+              </li>
+              <li className="flex items-center gap-1 text-zinc-400 text-sm">
+                <CheckCircle2 size={16} className="text-pink-400" />
+                Você completou <span className="text-zinc-100">&quot;Meditar&quot;</span> às &quot;
+                <span className="text-zinc-100">10h:12</span>&quot;
+                <button className="text-zinc-500 text-xs underline self-end">Desfazer</button>
+              </li>
+            </ul>
+          </article>
         </div>
       </div>
     </main>
